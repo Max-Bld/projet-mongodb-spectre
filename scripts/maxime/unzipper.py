50f14a763851480536a2aa775fcdfe11ccd0afab
Max-Bld <mblanchard95@gmail.com>
2023-06-30 00:12:01 +0200

    unzipper improved

--- scripts/maxime/unzipper.py
+++ scripts/maxime/unzipper.py
@@ -9,20 +9,23 @@ import os
 import pathlib
 import zipfile
 
-path = "C:/Users/digi/maxime_blanchard/projet_spectre/data/"
+    # Get current working path and create unzipped files directory
 
-file_list = os.listdir(path)
+split_word = "b-spectre"
+root = (__file__.split(split_word)[0])
+path_to_zip = fr"{root}b-spectre\assets\theremin\zip".replace( "\\", "/")
+path_to_extract = fr"{root}b-spectre\assets\theremin\pitched".replace( "\\", "/")
 
-path_to_extract = "C:/Users/digi/maxime_blanchard/projet_spectre/sounds/"
+file_list = os.listdir(path_to_zip)
 
+    # Execute the loop if the unzipped files directory is empty
 
-#%%
-
-for n in range(len(file_list)): # for looping through the whole file_list
-
-    file_name = path + file_list[n]
-    sub_folder_path = (path_to_extract + file_list[n]).replace('.zip', '')
-    os.mkdir(sub_folder_path)
-    with zipfile.ZipFile(file_name, 'r') as file_to_unzip:
-        file_to_unzip.printdir()
-        file_to_unzip.extractall(sub_folder_path)
+if len(os.listdir(path_to_extract)) == 0:
+    for n in range(len(file_list)): 
+    
+        zip_file = f"{path_to_zip}/{file_list[n]}"
+        sub_folder_path = f"{path_to_extract}/{file_list[n]}".replace('.zip', '')
+        os.mkdir(sub_folder_path)
+        with zipfile.ZipFile(zip_file, 'r') as file_to_unzip:
+            file_to_unzip.printdir()
+            file_to_unzip.extractall(sub_folder_path)
